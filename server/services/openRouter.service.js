f22ed639d3c4d1d3a665383bdd340a2501500c10
jyoti-kumari-singh20 <jyotikumarisingh881@gmail.com>
2026-05-25 23:48:52 +0530

    files created

---
+++ server/services/openRouter.service.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const askAI=async(messages)=>{
+    try{
+        if(!messages || !Array.isArray(messages) || messages.length===0){
+            throw new Error("Message array is empty.");   
+        }
+        const response=await axios.post(https://openrouter.ai/api/v1/chat/completions),
+        {
+            model: "openai/gpt-4o-mini",
+            messages:messages
+        },
+    )
+    }catch(error){
+
+    }
+}
+
