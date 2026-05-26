adfaa79c66d06bea6c6e6b07821c69c18f95dd79
jyoti-kumari-singh20 <jyotikumarisingh881@gmail.com>
2026-05-26 23:38:30 +0530

    file updated

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js";
+import interviewRouter from "./routes/interview.route.js";
 
 dotenv.config()
 
@@ -23,6 +24,7 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.use("/api/interview",interviewRouter)
 
 const PORT=process.env.PORT || 6000
 app.listen(PORT,()=>{
